af8f451f14a07e8ce00368033ebb4cff8e567bd7
tea-LZL <zhilong.k.liang@proton.me>
2026-02-12 02:50:02 +0200

    update background and intro glass

--- src/app/component/projects/projects.component.ts
+++ src/app/component/projects/projects.component.ts
@@ -1,46 +1,45 @@
-import { NgClass } from '@angular/common';
-import { AfterViewInit, Component, OnInit } from '@angular/core';
-import { Project } from '../../interfaces/project';
+import { NgClass } from "@angular/common";
+import { AfterViewInit, Component, OnInit } from "@angular/core";
+import { Project } from "../../interfaces/project";
 
 @Component({
-  selector: 'app-projects',
+  selector: "app-projects",
   imports: [NgClass],
-  templateUrl: './projects.component.html',
-  styleUrl: './projects.component.scss'
+  templateUrl: "./projects.component.html",
+  styleUrl: "./projects.component.scss",
 })
-export class ProjectsComponent implements AfterViewInit, OnInit{
-
-  constructor() {
-  }
+export class ProjectsComponent implements AfterViewInit, OnInit {
+  constructor() {}
   ngOnInit(): void {
-    const myCarouselElement = document.querySelector('#projectCarousel')
+    const myCarouselElement = document.querySelector("#projectCarousel");
     // this.imgLoadStatus = this.arrCarouselImg.map(() => false);
     // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
     const carousel = new (window as any).bootstrap.Carousel(myCarouselElement, {
       interval: 2000,
-      touch: false
-    })
+      touch: false,
+    });
   }
 
   ngAfterViewInit(): void {
     setTimeout(() => {
-      const loadingEl = document.getElementById('app-loading');
+      const loadingEl = document.getElementById("app-loading");
       if (loadingEl) {
-        loadingEl.classList.add('fade-out');
+        loadingEl.classList.add("fade-out");
         setTimeout(() => loadingEl.remove(), 500);
       }
     }, 275);
   }
 
-
   arrProjects: Project[] = [
     {
       Title: "Weathering With GO API",
       Cover_Image_URL: "assets/weathering_with_go_api_cover_image.png",
-      GitHub_Link: "https://github.com/tea-steeping-studio/weathering-with-go-api",
-      ShortDescription: "A modern, high-performance weather API built with Go and the Gin web framework. Get current weather conditions and forecasts for any location worldwide using the OpenWeatherMap API.",
-      Description: "Currently WIP cleaning up the solution, implementing a CI/CD solution to releases and constructing a separate frontend UI built with Electron"
-    }
+      GitHub_Link:
+        "https://github.com/tea-steeping-studio/weathering-with-go-api",
+      ShortDescription:
+        "A modern, high-performance weather API built with Go and the Gin web framework. Get current weather conditions and forecasts for any location worldwide using the OpenWeatherMap API.",
+      Description: `Weathering With GO API was the project I set out to understand the quirk and super power of the programing language Go when pair with Gin framework, through this journey, working with the simplesitic philosophy of the Go design and combining it with the ever so complicated web, it has thought me that working with small building block and creating something bit by bit is not a hurdle of inconvinience but a tool of precision to more so control and specify the need and want of an application. The project now hosted on Google Cloud and managed with CI/CD Dcoker, it gives me great leverage to update and modify when I get even the shortest free time with no hastle`,
+    },
     // ,{
     //   Title: "C# AutoClicker",
     //   Cover_Image_URL: "assets/weathering_with_go_api_cover_image.png",
