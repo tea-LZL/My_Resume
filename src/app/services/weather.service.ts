8c13b14566656dbd79edfd91c566b2a652aea4e4
tea-LZL <zhilong.k.liang@proton.me>
2026-02-13 18:41:18 +0200

    update to use API key when requesting for weather over net

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -2,21 +2,23 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable, catchError, throwError } from "rxjs";
 import { WeatherResponse } from "../interfaces/weather";
+import { environment } from "../../environments/environment";
 
 @Injectable({
   providedIn: "root",
 })
 export class WeatherService {
-  private apiUrl = ""; // To be set by user
+  private apiUrl = environment.OPENWEATHER_API_URL; // To be set by user
+  private key: string = environment.OPENWEATHER_API_KEY;
 
   constructor(private http: HttpClient) {}
 
   getWeather(params?: Record<string, string>): Observable<WeatherResponse> {
     return this.http
-      .get<WeatherResponse>(
-        "https://weathering-with-go-api-324269546067.europe-west1.run.app/api/v1/weather/current",
-        { params },
-      )
+      .get<WeatherResponse>(this.apiUrl, {
+        params,
+        headers: { "X-API-Key": this.key },
+      })
       .pipe(
         catchError((error) => {
           console.error("Weather API Error:", error);
