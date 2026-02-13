6ebf9e816c47d561ed798fbcc74f73842b19299b
tea-LZL <zhilong.k.liang@proton.me>
2026-02-13 18:50:26 +0200

    update environemnt to grab key from env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  OPENWEATHER_API_KEY: "_OPENWEATHER_API_KEY_",
+  OPENWEATHER_API_KEY: process.env["OPENWEATHER_API_KEY"],
   OPENWEATHER_API_URL:
     "https://weathering-with-go-api-324269546067.europe-west1.run.app/api/v1/weather/current",
 };
